daf9248f5b971505fd8530ce30961549cd862a12
ruifang <ruifang.li09@gmail.com>
2022-07-11 14:06:12 -0600

    initial form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CheckoutModel } from './checkout.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forms';
+  model = new CheckoutModel();
+
+  checkout() {
+    console.log('checking out', this.model);
+  }
 }
---
+++ src/app/checkout.model.ts
@@ -0,0 +1,3 @@
+export class CheckoutModel {
+  instructions?: string;
+}
