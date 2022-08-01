7b8b53038b26bf4a942e956794df324fdd4c767e
ruifang <ruifang.li09@gmail.com>
2022-08-01 10:28:40 -0600

    example of mailing address as nested component

--- src/app/mailing-address/mailing-address.component.ts
+++ src/app/mailing-address/mailing-address.component.ts
@@ -1,18 +1,15 @@
 import { Component, Input, ViewChild } from '@angular/core';
-import { ControlContainer, NgForm } from '@angular/forms';
+import { ControlContainer, NgForm, NgModelGroup } from '@angular/forms';
 import { AddressModel } from '../models';
 
 @Component({
   selector: 'app-mailing-address',
   templateUrl: './mailing-address.component.html',
   styleUrls: ['./mailing-address.component.scss'],
-  viewProviders: [ { provide: ControlContainer, useExisting: NgForm } ],
+  viewProviders: [ { provide: ControlContainer, useExisting: NgModelGroup } ],
 })
 export class MailingAddressComponent {
-  @ViewChild('mailingAddressForm') mailingAddressForm!: NgForm;
-  @Input() model: AddressModel;
+  @Input() model!: AddressModel;
 
-  constructor() {
-    this.model = new AddressModel();
-  }
+  constructor() {}
 }
