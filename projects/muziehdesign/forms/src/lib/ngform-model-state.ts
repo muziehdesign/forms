e718f175081953731c486aee239f809f1d99d33a
Ruifang Li <ruifang.li09@gmail.com>
2022-08-12 11:52:00 -0600

    log changes

--- projects/muziehdesign/forms/src/lib/ngform-model-state.ts
+++ projects/muziehdesign/forms/src/lib/ngform-model-state.ts
@@ -1,24 +1,20 @@
-import { NgForm, ValidationErrors } from "@angular/forms";
-import { BehaviorSubject, from, switchMap } from "rxjs";
-import { FieldError } from "./field-error";
-import { ModelStateOptions } from "./model-state-options";
-import { ModelStateResult } from "./model-state-result";
-import { ModelValidator } from "./model-validator";
+import { FormArray, FormGroup, NgForm, NgModelGroup, ValidationErrors } from '@angular/forms';
+import { BehaviorSubject, distinctUntilChanged, from, switchMap } from 'rxjs';
+import { FieldError } from './field-error';
+import { ModelStateOptions } from './model-state-options';
+import { ModelStateResult } from './model-state-result';
+import { ModelValidator } from './model-validator';
 
 export class NgFormModelState<T> {
   private changesSubject = new BehaviorSubject<ModelStateResult<T> | undefined>(undefined);
   public readonly changes = this.changesSubject.asObservable();
-  private errors: BehaviorSubject<FieldError[]> = new BehaviorSubject<FieldError[]>([]);
-  private errors$ = this.errors.asObservable();
-  private options?: ModelStateOptions;
 
-  constructor(private form: NgForm, private modelValidator: ModelValidator<T>, options?: ModelStateOptions) {
-    this.options = options;
-
+  constructor(private form: NgForm, private modelValidator: ModelValidator<T>, private options?: ModelStateOptions) {
     this.form.form.valueChanges
       .pipe(
+        distinctUntilChanged(),
         switchMap(async (x) => {
-          return from(this.validate())
+          return from(this.validate());
         })
       )
       .subscribe();
@@ -45,18 +41,23 @@ export class NgFormModelState<T> {
 
   // TODO: rename
   isValid(): boolean {
-    return this.errors.value.length == 0;
+    return this.changesSubject.value?.errors.length == 0;
   }
 
   setErrors(errors: FieldError[]) {
     //this.errors.next(errors);
-    throw new Error("needs implementation");
+    throw new Error('needs implementation');
   }
 
   async validate(): Promise<ModelStateResult<T>> {
+    console.log('validating');
     const model = this.form.value;
     const state = await this.runValidations(model, this.options?.onValidate);
-    this.form.resetForm(model);
+    Object.keys(this.form.controls).forEach((key) => {
+      const control = this.form.controls[key];
+      console.log('clearing errors for ' + key, control instanceof FormGroup, control instanceof FormArray);
+      this.form.controls[key].setErrors(null);
+    });
 
     const grouped = state.errors.reduce((grouped, v) => grouped.set(v.path, [...(grouped.get(v.path) || []), v]), new Map<string, FieldError[]>());
     grouped.forEach((value, path) => {
@@ -76,20 +77,15 @@ export class NgFormModelState<T> {
     return state;
   }
 
-  private async runValidations<T>(model:T, callback?: (list: FieldError[]) => FieldError[]): Promise<ModelStateResult<T>> {
+  private async runValidations<T>(model: T, callback?: (list: FieldError[]) => FieldError[]): Promise<ModelStateResult<T>> {
     //this.removeCurrentErrors();
     const list = await this.modelValidator.validate(model);
     const final = callback?.(list) || list;
     return {
-      valid: this.errors.value.length == 0,
-      errors: final, 
-      model: model
+      valid: final.length === 0,
+      errors: final,
+      model: model,
     };
     // this.errors.next(final);
   }
-
-  private removeCurrentErrors() {
-    //Object.keys(this.form.controls).forEach((key) => this.form.controls[key].setErrors(null));
-  }
-
 }
