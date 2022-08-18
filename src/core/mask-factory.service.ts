eb63f229a9ae773c549f1b17c0f8b20eb91bfebf
Sebas Mendez <sebasmendezz17@gmail.com>
2022-08-18 11:58:42 -0600

    add mask factory service

---
+++ src/core/mask-factory.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { IMaskFactory } from 'angular-imask';
+import IMask from 'imask';
+import * as moment from 'moment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class MaskFactoryService extends IMaskFactory {
+    create<Opts extends IMask.AnyMaskedOptions>(el: IMask.MaskElement | HTMLElement, opts: Opts): IMask.InputMask<Opts> {
+        if (opts.mask === 'phone') {
+            const phoneOpts = {};
+            const finalOptions = Object.assign({}, phoneOpts, opts);
+            finalOptions.mask = '(000) 000-0000';
+            return IMask(el, finalOptions);
+        }
+
+        if (opts.mask === 'currency') {
+            const currencyOpts = {};
+            const finalOptions = Object.assign({}, currencyOpts, opts);
+            finalOptions.mask = [
+                { mask: '' },
+                {
+                    mask: 'num',
+                    lazy: false,
+                    blocks: {
+                        num: {
+                            mask: Number,
+                            scale: 2,
+                            thousandsSeparator: ',',
+                            padFractionalZeros: true,
+                            radix: '.',
+                            mapToRadix: ['.'],
+                        },
+                    },
+                },
+            ];
+            return IMask(el, finalOptions);
+        }
+
+        if (opts.mask === 'date') {
+            const format = 'MM/DD/YYYY';
+            const dateOpts = {
+                mask: Date,
+                pattern: format,
+                format: (date: Date) => moment(date).format(format),
+                parse: (str: string) => moment(str, format),
+                blocks: {
+                    // eslint-disable-next-line @typescript-eslint/naming-convention
+                    MM: {
+                        mask: IMask.MaskedRange,
+                        from: 1,
+                        to: 12,
+                    },
+                    // eslint-disable-next-line @typescript-eslint/naming-convention
+                    DD: {
+                        mask: IMask.MaskedRange,
+                        from: 1,
+                        to: 31,
+                    },
+                    // eslint-disable-next-line @typescript-eslint/naming-convention
+                    YYYY: {
+                        mask: IMask.MaskedRange,
+                        from: -9999,
+                        to: 9999,
+                    },
+                },
+            };
+            const finalOptions = Object.assign({}, dateOpts, opts);
+            finalOptions.mask = Date;
+            return IMask(el, finalOptions);
+        }
+        return IMask(el, opts);
+    }
+}
