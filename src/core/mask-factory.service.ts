35921545f439b95057b4de50e05fea4240ce5c6b
Sebas Mendez <sebasmendezz17@gmail.com>
2022-08-19 12:17:37 -0600

    use mask options from lib

--- src/core/mask-factory.service.ts
+++ src/core/mask-factory.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IMaskFactory } from 'angular-imask';
 import IMask from 'imask';
-import { dateMaskOptions } from '@muziehdesign/forms';
+import { currencyOptions, dateMaskOptions, phoneNumberOptions } from '@muziehdesign/forms';
 
 @Injectable({
   providedIn: 'root',
@@ -9,15 +9,13 @@ import { dateMaskOptions } from '@muziehdesign/forms';
 export class MaskFactoryService extends IMaskFactory {
   create<Opts extends IMask.AnyMaskedOptions>(el: IMask.MaskElement | HTMLElement, opts: Opts): IMask.InputMask<Opts> {
     if (opts.mask === 'phone') {
-      const phoneOpts = {};
-      const finalOptions = Object.assign({}, phoneOpts, opts);
+      const finalOptions = Object.assign({}, phoneNumberOptions, opts);
       finalOptions.mask = '(000) 000-0000';
       return IMask(el, finalOptions);
     }
 
     if (opts.mask === 'currency') {
-      const currencyOpts = {};
-      const finalOptions = Object.assign({}, currencyOpts, opts);
+      const finalOptions = Object.assign({}, currencyOptions, opts);
       finalOptions.mask = [
         { mask: '' },
         {
