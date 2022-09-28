8c98e671f437f4ddd00f4d40dc62ca641c206c51
Juan Gomez <jgomez@enerbankusa.com>
2022-09-28 14:46:49 -0600

    more tests

--- projects/muziehdesign/forms/src/lib/model-schema.factory.spec.ts
+++ projects/muziehdesign/forms/src/lib/model-schema.factory.spec.ts
@@ -11,51 +11,115 @@ describe('ModelSchemaFactory', () => {
     service = TestBed.inject(ModelSchemaFactory);
   });
 
-  // it('should go through happy path', async () => {
-  //   const builtFactory = service.build(new Car());
+  it('should go through happy path', async () => {
+    const builtFactory = service.build(new Car());
 
-  //   const carWithInvalidYear: Car = { topSpeed: 200, brand: 'Audi Audi Audi Audi Audi Audi Audi Audi Audi Audi Audi', year: '2010a' as unknown as number };
+    const validation = await builtFactory.validate({
+      topSpeed: 200,
+      brand: 'Audi',
+      year: 2010,
+      nextOilChange: new Date(new Date().getFullYear() + 5, 1, 1),
+      inscriptionDate: new Date(new Date().getFullYear() - 2, 1, 1),
+    } as Car);
 
-  //   const validation = await builtFactory.validate(carWithInvalidYear);
-
-  //   expect(service).toBeTruthy();
-  //   expect(validation.length).toBe(2);
-  //   expect(validation[0]).toEqual({ path: 'brand', type: 'length', message: 'Brand cannot exceed 200 characters' });
-  //   expect(validation[1]).toEqual({ path: 'year', type: 'matches', message: 'Please enter a valid year' });
-  // });
+    expect(service).toBeTruthy();
+    expect(validation.length).toBe(0);
+  });
 
   it('should create and validate regex', async () => {
     const builtFactory = service.build(new Car());
 
-    const carWithInvalidYear: Car = { topSpeed: 200, brand: 'Audi ', year: '2010a' as unknown as number };
-
-    const validation = await builtFactory.validate(carWithInvalidYear);
+    const validation = await builtFactory.validate({
+      topSpeed: 200,
+      brand: 'Audi ',
+      year: '201a' as unknown as number,
+      nextOilChange: new Date(new Date().getFullYear() + 5, 1, 1),
+    } as Car);
 
     expect(service).toBeTruthy();
-    expect(validation.length).toBe(1);
-    expect(validation[0]).toEqual({ path: 'year', type: 'matches', message: 'Please enter a valid year' });
+    expect(validation).toEqual([{ path: 'year', type: 'matches', message: 'Please enter a valid year' }]);
   });
 
-  it('should create and validate maxlength', async () => {
+  it('should validate maxlength', async () => {
     const builtFactory = service.build(new Car());
 
-    const carWithInvalidYear: Car = { topSpeed: 200, brand: 'Audi '.repeat(500), year: 2010 };
-
-    const validation = await builtFactory.validate(carWithInvalidYear);
+    const validation = await builtFactory.validate({
+      topSpeed: 200,
+      brand: 'Audi '.repeat(500),
+      year: 2010,
+      nextOilChange: new Date(new Date().getFullYear() + 5, 1, 1),
+    } as Car);
 
     expect(service).toBeTruthy();
-    expect(validation.length).toBe(1);
-    expect(validation[0]).toEqual({ path: 'brand', type: 'max', message: 'Brand cannot exceed 200 characters' });
+    expect(validation).toEqual([{ path: 'brand', type: 'max', message: 'Brand cannot exceed 200 characters' }]);
   });
 
   it('should validate required', async () => {
     const builtFactory = service.build(new Car());
+    const validation = await builtFactory.validate({ topSpeed: 200, year: 2010 } as Car);
 
-    const carWithInvalidUnprovidedSpeed: Car = { brand: 'Audi', year: 2010 };
+    expect(validation).toEqual([{ path: 'brand', type: 'required', message: 'Please enter a valid brand' }]);
+  });
 
-    const validation = await builtFactory.validate(carWithInvalidUnprovidedSpeed);
+  it('should validate minlength', async () => {
+    const builtFactory = service.build(new Car());
 
-    expect(validation.length).toBe(1);
-    expect(validation[0]).toEqual({ path: 'topSpeed', type: 'required', message: 'Please enter a valid top speed' });
+    const validation = await builtFactory.validate({ topSpeed: 200, brand: 'a', year: 2010 } as Car);
+
+    expect(validation).toEqual([{ path: 'brand', type: 'min', message: 'Brand requires at least 2 characters' }]);
+  });
+
+  it('should validate maxlength and length simultaneously', async () => {
+    const builtFactory = service.build(new Car());
+
+    const validation = await builtFactory.validate({ brand: 'Audi '.repeat(200), year: 123 } as Car);
+
+    const sortedErrors = validation.sort((a, b) => a.path.localeCompare(b.path));
+
+    expect(sortedErrors).toEqual([
+      { path: 'brand', type: 'max', message: 'Brand cannot exceed 200 characters' },
+      { path: 'topSpeed', type: 'required', message: 'Please enter a valid top speed' },
+      { path: 'year', type: 'length', message: 'Please enter a valid year' },
+    ]);
+  });
+
+  it('should validate min date', async () => {
+    const builtFactory = service.build(new Car());
+
+    const validation = await builtFactory.validate({
+      topSpeed: 200,
+      brand: 'Toyota',
+      year: 2010,
+      inscriptionDate: new Date(1799, 1, 1),
+    } as Car);
+
+    expect(validation).toEqual([{ path: 'inscriptionDate', type: 'min', message: 'Please enter a valid inscription date' }]);
+  });
+
+  it('should validate min date', async () => {
+    const builtFactory = service.build(new Car());
+
+    const validation = await builtFactory.validate({
+      topSpeed: 200,
+      brand: 'Toyota',
+      year: 2010,
+      inscriptionDate: new Date(new Date().getFullYear() + 5, 1, 1),
+    } as Car);
+
+    expect(validation).toEqual([{ path: 'inscriptionDate', type: 'max', message: 'Please enter a valid inscription date' }]);
+  });
+
+  it('should validate date test', async () => {
+    const builtFactory = service.build(new Car());
+
+    const validation = await builtFactory.validate({
+      topSpeed: 200,
+      brand: 'Toyota',
+      year: 2010,
+      inscriptionDate: new Date(new Date().getFullYear() - 2, 1, 1),
+      nextOilChange: new Date(new Date().getFullYear() - 2, 1, 1),
+    } as Car);
+
+    expect(validation).toEqual([{ path: 'nextOilChange', type: 'nextOilChange', message: 'Please enter a valid oil change' }]);
   });
 });
--- projects/muziehdesign/forms/src/lib/test-files/car.ts
+++ projects/muziehdesign/forms/src/lib/test-files/car.ts
@@ -1,12 +1,18 @@
-import { StringType, length, required, pattern, maxLength } from '../type-annotations';
+import { StringType, length, required, pattern, min, max, maxLength, minLength, DateType, test } from '../type-annotations';
 
 export class Car {
   @StringType(required('Please enter a valid top speed'), pattern(/[0-9]$/, 'Please enter a valid top speed'))
   topSpeed?: number;
 
-  @StringType(required('Please enter a valid brand'), maxLength(200, 'Brand cannot exceed 200 characters'))
+  @StringType(required('Please enter a valid brand'), minLength(2, 'Brand requires at least 2 characters'), maxLength(200, 'Brand cannot exceed 200 characters'))
   brand?: string;
 
-  @StringType(required('Please enter a valid year'), pattern(/[0-9]$/, 'Please enter a valid year'))
+  @StringType(required('Please enter a valid year'), length(4, 'Please enter a valid year'), pattern(/[0-9]$/, 'Please enter a valid year'))
   year?: number;
+
+  @DateType(test('nextOilChange', (d: Date) => !d || d > new Date(), 'Please enter a valid oil change'))
+  nextOilChange?: Date;
+
+  @DateType(min(new Date(1800, 1, 1), 'Please enter a valid inscription date'), max(new Date(), 'Please enter a valid inscription date'))
+  inscriptionDate?: Date;
 }
