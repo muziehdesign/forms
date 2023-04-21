8b41b06c15a39e18a88014e56d172729242531ca
alvartabe <alvartabe777@gmail.com>
2023-04-21 16:22:27 -0600

    new home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,11 @@ import { ApplicantComponent } from './examples/applicant/applicant.component';
 import { DateComponent } from './examples/date/date.component';
 import { NumberComponent } from './examples/number/number.component';
 import { StringComponent } from './examples/string/string.component';
-import { HomeComponent } from './home/home.component';
 import { ObjectComponent } from './examples/object/object.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'examples/applicant', component: ApplicantComponent },
+  { path: '',  redirectTo: 'examples/string', pathMatch: 'full' },
+  { path: 'examples/applicant', component: ApplicantComponent},
   { path: 'examples/string', component: StringComponent },
   { path: 'examples/number', component: NumberComponent },
   { path: 'examples/date', component: DateComponent },
