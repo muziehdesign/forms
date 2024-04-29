df90345927dd8b5747c3c2677264157bf8fd6fec
ruifang <ruifang.li09@gmail.com>
2024-04-29 14:42:26 -0600

    produce correct version number

--- version.sh
+++ version.sh
@@ -4,14 +4,14 @@ BRANCH=$2
 
 VERSION=$(npm pkg get version | tr -d '"')
 MAJOR=$(echo $VERSION | awk -F '[/.]' '{ print $1 }')
-ECHO $MAJOR
 MINOR=$(echo $VERSION | awk -F '[/.]' '{ print $2 }')
-ECHO $MINOR
 PATCH=$BUILD
-SUFFIX=''
-if [[ $BRANCH != "master" ]] && [[ $BRANCH != "develop" ]] && [[ $BRANCH =~ ^"release/" ]]
-then
-    SUFFIX='-alpha'
-fi
 
-npm version "${MAJOR}.${MINOR}.${PATCH:="0"}${SUFFIX}" --no-commit-hooks --no-git-tag-version
+ECHO $BRANCH
+
+if [[ $BRANCH == "master" ]] || [[ $BRANCH == "develop" ]] || [[ $BRANCH =~ "release/" ]]
+then
+    npm version "${MAJOR}.${MINOR}.${PATCH:="0"}" --no-commit-hooks --no-git-tag-version
+else
+    npm version "${MAJOR}.${MINOR}.${PATCH:="0"}-alpha.0" --no-commit-hooks --no-git-tag-version
+fi
