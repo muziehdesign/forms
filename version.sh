96d5430d397105e8682d0cb99edadcb47117440a
ruifang <ruifang.li09@gmail.com>
2024-04-29 15:04:22 -0600

    remove debug line

--- version.sh
+++ version.sh
@@ -7,11 +7,9 @@ MAJOR=$(echo $VERSION | awk -F '[/.]' '{ print $1 }')
 MINOR=$(echo $VERSION | awk -F '[/.]' '{ print $2 }')
 PATCH=$BUILD
 
-ECHO $BRANCH
-
 if [[ $BRANCH == "master" ]] || [[ $BRANCH == "develop" ]] || [[ $BRANCH =~ "release/" ]]
 then
     npm version "${MAJOR}.${MINOR}.${PATCH:="0"}" --no-commit-hooks --no-git-tag-version
 else
-    npm version "${MAJOR}.${MINOR}.${PATCH:="0"}-alpha.0" --no-commit-hooks --no-git-tag-version
+    npm version "${MAJOR}.${MINOR}.${PATCH:="0"}-beta" --no-commit-hooks --no-git-tag-version
 fi
