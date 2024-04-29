073563a8a0d2cdab63d692a85ee273374a946f2e
ruifang <ruifang.li09@gmail.com>
2024-04-29 15:35:07 -0600

    change syntax

--- version.sh
+++ version.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 BUILD=$1
 BRANCH=$2
 
 VERSION=$(npm pkg get version | tr -d '"')
-MAJOR=$(echo $VERSION | awk -F '[/.]' '{ print $1 }')
-MINOR=$(echo $VERSION | awk -F '[/.]' '{ print $2 }')
+MAJOR=$(echo "$VERSION" | awk -F '[/.]' '{ print $1 }')
+MINOR=$(echo "$VERSION" | awk -F '[/.]' '{ print $2 }')
 PATCH=$BUILD
 
-if [[ $BRANCH == "master" ]] || [[ $BRANCH == "develop" ]] || [[ $BRANCH =~ "release/" ]]
+if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "develop" ] ||  [ "$BRANCH" =~ "release/" ]
 then
     npm version "${MAJOR}.${MINOR}.${PATCH:="0"}" --no-commit-hooks --no-git-tag-version
 else
