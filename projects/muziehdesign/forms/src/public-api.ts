2fc2b7dbb7a459bab0cc05a8a674066e74641318
ruifang <ruifang.li09@gmail.com>
2025-03-13 19:13:15 -0600

    expose function

--- projects/muziehdesign/forms/src/public-api.ts
+++ projects/muziehdesign/forms/src/public-api.ts
@@ -13,6 +13,6 @@ export * from './lib/field-errors/field-errors.component';
 export * from './lib/masks';
 
 export { FieldSchema, FieldSchemaType, StringSchema, NumberSchema, DateSchema, BooleanSchema, FileSchema, ObjectSchema,
-    buildStringSchema, buildNumberSchema, buildDateSchema, buildBooleanSchema
+    buildStringSchema, buildNumberSchema, buildDateSchema, buildBooleanSchema, buildArraySchema
  } from './lib/field-schema';
 export { MzCheckboxGroup, FieldOption } from './lib/checkbox-group/checkbox-group.component';
